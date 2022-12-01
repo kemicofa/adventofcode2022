5daa935307e37ea45bf030ae7c53d6848eb208cf
Kevin Faulhaber <kevin.faulhaber@gmail.com>
2022-12-01 23:39:10 +0100

    refactor(day 1): helper function to read from input file

--- src/day_1_calorie_counting.rs
+++ src/day_1_calorie_counting.rs
@@ -22,11 +22,14 @@ mod tests {
             .collect()
     }
 
+    fn get_input_from_file(path: &str) -> String {
+        fs::read_to_string(path)
+            .expect("Should have been able to read the file")
+    }
+
     #[test]
     fn it_can_find_most_calories_for_example_puzzle_input() {
-        let input = fs::read_to_string("./data/day_1_calorie_counting_example_puzzle_input.txt")
-            .expect("Should have been able to read the file");
-
+        let input = get_input_from_file("./data/day_1_calorie_counting_example_puzzle_input.txt");
         let elves = input_to_vector_2d_i32(input);
 
         assert_eq!(calorie_counting(elves), 24000);
@@ -34,9 +37,7 @@ mod tests {
 
     #[test]
     fn it_can_find_most_calories_for_puzzle_input() {
-        let input = fs::read_to_string("./data/day_1_calorie_counting_puzzle_input.txt")
-            .expect("Should have been able to read the file");
-
+        let input = get_input_from_file("./data/day_1_calorie_counting_puzzle_input.txt");
         let elves = input_to_vector_2d_i32(input);
 
         assert_eq!(calorie_counting(elves), 71124);
