de2bf622149800195d28a49fba23b80e67170047
Kevin Faulhaber <kevin.faulhaber@gmail.com>
2022-12-03 09:12:04 +0100

    chore: renamed data files to day n

--- src/day_1_calorie_counting.rs
+++ src/day_1_calorie_counting.rs
@@ -29,7 +29,7 @@ mod tests {
 
     #[test]
     fn it_can_find_most_calories_for_example_puzzle_input() {
-        let input = get_input_from_file("./data/day_1_calorie_counting_example_puzzle_input.txt");
+        let input = get_input_from_file("./data/day_1_example_puzzle_input.txt");
         let elves = input_to_vector_2d_i32(input);
 
         assert_eq!(calorie_counting(elves), 24000);
@@ -37,7 +37,7 @@ mod tests {
 
     #[test]
     fn it_can_find_most_calories_for_puzzle_input() {
-        let input = get_input_from_file("./data/day_1_calorie_counting_puzzle_input.txt");
+        let input = get_input_from_file("./data/day_1_puzzle_input.txt");
         let elves = input_to_vector_2d_i32(input);
 
         assert_eq!(calorie_counting(elves), 71124);
--- src/day_2_rock_paper_scissors.rs
+++ src/day_2_rock_paper_scissors.rs
@@ -50,7 +50,7 @@ mod tests {
 
     #[test]
     fn it_can_use_the_strategy_guide_to_calculate_rps_score_for_example_puzzle_input() {
-        let input = get_input_from_file("./data/day_2_rock_paper_scissors_example_puzzle_input.txt");
+        let input = get_input_from_file("./data/day_2_example_puzzle_input.txt");
         let strategy_guide = input_to_vector_2d_char(input);
 
         assert_eq!(solve_rock_paper_scissors(strategy_guide), 15);
@@ -58,7 +58,7 @@ mod tests {
 
     #[test]
     fn it_can_use_the_strategy_guide_to_calculate_rps_score_for_puzzle_input() {
-        let input = get_input_from_file("./data/day_2_rock_paper_scissors_puzzle_input.txt");
+        let input = get_input_from_file("./data/day_2_puzzle_input.txt");
         let strategy_guide = input_to_vector_2d_char(input);
 
         assert_eq!(solve_rock_paper_scissors(strategy_guide), 15572);
