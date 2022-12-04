9351a7c14903aa8fe722e78355427cd2081bf014
Kevin Faulhaber <kevin.faulhaber@gmail.com>
2022-12-04 22:21:26 +0100

    feat(day 3): part 2

--- src/day_3_rucksack_reorganisation.rs
+++ src/day_3_rucksack_reorganisation.rs
@@ -1,4 +1,6 @@
-use std::{collections::HashSet, str::Chars};
+use std::{collections::{HashSet, HashMap}, str::Chars, hash::Hash};
+
+// Part 1
 
 const UPPERCASE_ASCII_OFFSET: u32 = 38;
 const LOWERCASE_ASCII_OFFSET: u32 = 96;
@@ -37,6 +39,46 @@ pub fn reorganise_rucksack(rucksacks: Vec<(String, String)>) -> u32 {
     item_types_to_sum_of_priorities(duplicate_item_types)
 }
 
+// Part 2
+
+pub fn find_badges_and_sum_priorities(rucksacks: Vec<String>) -> u32 {
+
+    const SLICE_LENGTH: usize = 3;
+
+    let mut i: usize = 0;
+    let mut total: u32 = 0;
+    loop {
+        let mut map: HashMap<char, u32> = HashMap::new();
+        let rucksack_group = &rucksacks[i..i + SLICE_LENGTH];
+        for rucksack in rucksack_group {
+            let mut seen: HashSet<char> = HashSet::new();
+            for item_type in rucksack.chars() {
+                if seen.contains(&item_type) {
+                    continue;
+                }
+                seen.insert(item_type);
+                let next_count = match map.get(&item_type) {
+                    Some(current_count) => current_count + 1,
+                    None => 1
+                };
+                map.insert(item_type, next_count);
+            }
+        }
+        i += SLICE_LENGTH;
+        total += item_types_to_sum_of_priorities(
+            map
+                .into_iter()
+                .filter(|(_, value)| *value == 3)
+                .map(|(key, _)| key)
+                .collect()
+        );
+        if i >= rucksacks.len() {
+            break;
+        }
+    }
+    total
+}
+
 #[cfg(test)]
 mod tests {
     use std::fs;
@@ -47,6 +89,13 @@ mod tests {
             .split("\n")
             .map(|rucksack| rucksack.split_at(rucksack.len()/2))
             .map(|(compartment1, compartment2)| (String::from(compartment1), String::from(compartment2)))
+            .collect()
+    }
+
+    fn input_to_vector_str(input: String) -> Vec<String> {
+        input
+            .split("\n")
+            .map(|rucksack| String::from(rucksack))
             .collect()
     }
 
@@ -68,5 +117,13 @@ mod tests {
         let reorganise_rucksacks = input_to_vector_tuple_str(input);
 
         assert_eq!(reorganise_rucksack(reorganise_rucksacks), 7980);
+    }
+
+    #[test]
+    fn it_should_find_sum_of_badges_with_priority_for_puzzle_input() {
+        let input = get_input_from_file("./data/day_3_puzzle_input.txt");
+        let reorganise_rucksacks = input_to_vector_str(input);
+
+        assert_eq!(find_badges_and_sum_priorities(reorganise_rucksacks), 2881);
     }
 }
